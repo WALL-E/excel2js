4489826e5226132b5e58ee770fdd124f63306470
zhangzheng <zhangzheng@qianbao.com>
2017-01-20 16:12:45 +0800

    增加excel2js.py

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # excel2js
+可以把excel文件转换为js文件
+
+## excel内容格式
+必须符合Python的基本语法，字符串可以不写引号
+
+## 用法
+excel2js.py excel\_file jsi\_file
---
+++ excel2js.py
@@ -0,0 +1,73 @@
+#!/usr/local/bin/python2.7
+#-*- coding: utf8 -*-
+
+import os
+import sys
+import json
+import ast
+
+import xlrd
+ 
+
+def convert(input, output):
+    bk = xlrd.open_workbook(input)
+    shxrange = range(bk.nsheets)
+    try:
+        sh = bk.sheet_by_name("Sheet1")
+    except:
+        print "no sheet in %s named Sheet1" % fname
+
+#获取行数
+    nrows = sh.nrows
+#获取列数
+    ncols = sh.ncols
+#print "nrows %d, ncols %d" % (nrows,ncols)
+
+    cases = []
+#获取各行数据
+    for i in range(1, nrows):
+        case = {}
+        for j in range(0, ncols):
+            cell_key = sh.cell_value(0, j)
+            if isinstance(cell_key, unicode):
+                cell_key = cell_key.encode("utf-8")
+            cell_val = sh.cell_value(i, j)
+            if isinstance(cell_val, unicode):
+                cell_val = sh.cell_value(i, j).encode("utf-8")
+                cell_val = "".join(cell_val.split("\n"))
+                try:
+                    cell_val = ast.literal_eval(cell_val)
+                except SyntaxError:
+                    pass
+                except ValueError:
+                    pass
+            case[cell_key] = cell_val
+        cases.append(case)
+
+    with open(output, "w") as file:
+        file.write("\n")
+        file.write("cases = ")
+        dump = json.dumps(cases, indent=4, separators=(',', ': '), ensure_ascii=False)
+        file.write(dump)
+        file.write(";")
+        file.write("\n\n")
+        file.write("module.exports = cases;")
+
+
+def main():
+    if len(sys.argv) < 3:
+        print "%s excel_file js_file" % (sys.argv[0])
+        sys.exit(1)
+
+    excel = sys.argv[1]
+    js = sys.argv[2]
+
+    if not os.path.exists(excel):
+        print "%s is not exists" % (excel)
+        sys.exit(2)
+
+    convert(excel, js)
+
+
+if __name__ == '__main__':
+    main()
